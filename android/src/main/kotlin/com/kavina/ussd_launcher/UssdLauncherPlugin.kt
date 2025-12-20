36c33971622c3f0921ed57954b971670f6e33703
Subrato Saha <107150311+subrato-saha@users.noreply.github.com>
2025-12-20 23:28:25 +0600

    Add interactiveMode variable to UssdLauncherPlugin

--- android/src/main/kotlin/com/kavina/ussd_launcher/UssdLauncherPlugin.kt
+++ android/src/main/kotlin/com/kavina/ussd_launcher/UssdLauncherPlugin.kt
@@ -16,6 +16,7 @@ class UssdLauncherPlugin : FlutterPlugin, MethodCallHandler {
     private lateinit var context: Context
     private lateinit var ussdSessionUnique: UssdSessionUnique
     private lateinit var ussdMultiSession: UssdMultiSession
+    private var interactiveMode = false
 
     companion object {
         private var methodChannel: MethodChannel? = null
